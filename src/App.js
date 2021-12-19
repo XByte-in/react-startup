ceadae5c712c71317a7982e651dc85853884c915
Pranshu-bst <pranshu.gupta@bluestacks.com>
2021-12-19 18:43:04 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Text from './common-lib/Text';
 function App() {
   return (
     <div className="App">
+      <Text text="test"/>
     </div>
   );
 }
--- src/common-lib/button/index.js
+++ src/common-lib/button/index.js
@@ -1,10 +1,9 @@
-import { Text } from "../text";
 import "./index.scss";
-
+import Text from "../Text";
 const Button = ({ btnText }) => {
   return (
     <button>
-      <Text text={btnText} />
+      <Text text="test"/>
     </button>
   );
 };
