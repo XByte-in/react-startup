4b9f3ca65202185684f5ff03ddd91700407a579b
Pranshu-bst <Pranshu.Gupta@Bluestacks.com>
2022-10-03 19:25:09 +0530

    Updated

--- src/common/pictures.tsx
+++ src/common/pictures.tsx
@@ -1,24 +1,24 @@
-import hibiscus from "./pictures/hibiscus.png";
-import vines from "./pictures/vines.png";
-import waterfall from "./pictures/waterfall.png";
+// import hibiscus from "./pictures/hibiscus.png";
+// import vines from "./pictures/vines.png";
+// import waterfall from "./pictures/waterfall.png";
 
 export const Icons = {
-  hibiscus: hibiscus,
-  vines: vines,
-  waterfall: waterfall,
+  // hibiscus: hibiscus,
+  // vines: vines,
+  // waterfall: waterfall,
 };
 
 export enum IconSize {
-  _16 = 16,
-  _24 = 24,
-  _32 = 32,
-  _48 = 48,
-  _64 = 64,
+  // _16 = 16,
+  // _24 = 24,
+  // _32 = 32,
+  // _48 = 48,
+  // _64 = 64,
 }
 
 
 export const Images = {
-  hibiscus: hibiscus,
-  vines: vines,
-  waterfall: waterfall,
+  // hibiscus: hibiscus,
+  // vines: vines,
+  // waterfall: waterfall,
 };
