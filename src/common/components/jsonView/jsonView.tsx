26cad3af8681ee0eae05f51903191ef6b7822252
Pranshu-bst <Pranshu.Gupta@Bluestacks.com>
2022-10-07 18:59:57 +0530

    Updated

--- src/common/components/jsonView/jsonView.tsx
+++ src/common/components/jsonView/jsonView.tsx
@@ -11,16 +11,110 @@ interface IJsonViewParam {
   customFormatter?: any;
 }
 
-export const createFormattedTable = (
+const getFormattedString = (stringData: any) => {
+  const jsonValueType = CommonUtils.jsonValueType(stringData);
+  if (
+    jsonValueType === JsonValueType.null ||
+    jsonValueType === JsonValueType.undefined
+  )
+    return <></>;
+  else if (
+    jsonValueType === JsonValueType.boolean ||
+    jsonValueType === JsonValueType.number
+  )
+    stringData = stringData.toString();
+  else stringData = CommonUtils.convertUrlInString(stringData);
+  return (
+    <span
+      key={Math.random().toString()}
+      className="td-string"
+      dangerouslySetInnerHTML={{ __html: stringData }}
+    ></span>
+  );
+};
+
+const getFormattedData = (props: any, parentKey: string, data: any) => {
+  if (Object.keys(props.customFormatter).includes(parentKey))
+    return props.customFormatter[parentKey](data);
+  const dataType = CommonUtils.jsonValueType(data);
+  let result;
+  if (dataType === JsonValueType.object)
+    result = getFormattedObject(props, parentKey, data);
+  else if (dataType === JsonValueType.array)
+    result = getFormattedArray(props, parentKey, data);
+  else result = getFormattedString(data);
+  return result;
+};
+
+const getFormattedArray = (props: any, parentKey: string, arrayData: any) => {
+  const divContent: any = arrayData.map((arrayItem: any) =>
+    getFormattedData(props, parentKey, arrayItem)
+  );
+  return <>{divContent}</>;
+};
+
+const getFormattedObject = (
+  props: any,
+  parentKey: string,
+  jsonData: { [key: string]: any }
+) => {
+  const keys = Object.keys(jsonData);
+  if (keys.length === 0) return <></>;
+
+  const tableContent = keys.map((key) =>
+    getFormattedTableRow(props, parentKey, key, jsonData[key])
+  );
+  return (
+    <table key={`${parentKey}.${Math.random()}`}>
+      <thead>
+        <tr key={Math.random()}>
+          <th style={{ width: 175 }}>FIELD</th>
+          <th>VALUE</th>
+        </tr>
+      </thead>
+      <tbody>{tableContent}</tbody>
+    </table>
+  );
+};
+
+const getFormattedTableRow = (
+  props: any,
+  parentKey: string,
+  field: string,
+  value: any
+) => {
+  let clsName = "";
+  if (
+    CommonUtils.jsonValueType(value) === JsonValueType.string ||
+    CommonUtils.jsonValueType(value) === JsonValueType.boolean ||
+    CommonUtils.jsonValueType(value) === JsonValueType.number
+  )
+    clsName = "td-string";
+  let fieldKey = field;
+  if (parentKey !== "") fieldKey = `${parentKey}.${field}`;
+  const formattedVal = getFormattedData(props, fieldKey, value);
+  return (
+    <tr key={Math.random()}>
+      <td className="td-field td-string">{field}</td>
+      <td>
+        <span className={`value-cell ${clsName}`}>{formattedVal}</span>
+      </td>
+    </tr>
+  );
+};
+
+export const createFormattedTable = (  
   columns: Array<string>,
   jsonData: Array<any>
 ) => {
   const tableColumns = columns.map((col) => <th key={Math.random()}>{col}</th>);
-  const tableData = jsonData.map((rowData: { [key: string]: string }) => {    
+  const tableData = jsonData.map((rowData: { [key: string]: any }) => {
     const rowsData = columns.map((col) => {
       return (
-        <td key={Math.random()}>
-          <span className="td-string">{rowData[col]}</span>
+        <td key={Math.random()} className="td-string">
+          <span className="td-string">
+            {getFormattedString(rowData[col])}
+          </span>
         </td>
       );
     });
@@ -46,94 +140,9 @@ const JsonView = (props: IJsonViewParam) => {
       props.topElements,
       props.bottomElements
     );
-    setFormattedJsonObject(getFormattedData("", orderedResponse));
+    setFormattedJsonObject(getFormattedData(props, "", orderedResponse));
   }, [props.jsonObject]);
 
-  function getFormattedData(parentKey: string, data: any) {
-    if (Object.keys(props.customFormatter).includes(parentKey))
-      return props.customFormatter[parentKey](data);
-    const dataType = CommonUtils.jsonValueType(data);
-    let result;
-    if (dataType === JsonValueType.object)
-      result = getFormattedObject(parentKey, data);
-    else if (dataType === JsonValueType.array)
-      result = getFormattedArray(parentKey, data);
-    else result = getFormattedString(data);
-    return result;
-  }
-
-  function getFormattedString(stringData: any) {
-    const jsonValueType = CommonUtils.jsonValueType(stringData);
-    if (
-      jsonValueType === JsonValueType.null ||
-      jsonValueType === JsonValueType.undefined
-    )
-      return <></>;
-    else if (
-      jsonValueType === JsonValueType.boolean ||
-      jsonValueType === JsonValueType.number
-    )
-      stringData = stringData.toString();
-    else stringData = CommonUtils.convertUrlInString(stringData);
-    return (
-      <span
-        key={Math.random().toString()}
-        className="td-string"
-        dangerouslySetInnerHTML={{ __html: stringData }}
-      ></span>
-    );
-  }
-
-  function getFormattedArray(parentKey: string, arrayData: any) {
-    const divContent: any = arrayData.map((arrayItem: any) =>
-      getFormattedData(parentKey, arrayItem)
-    );
-    return <>{divContent}</>;
-  }
-
-  function getFormattedObject(
-    parentKey: string,
-    jsonData: { [key: string]: any }
-  ) {
-    const keys = Object.keys(jsonData);
-    if (keys.length === 0) return <></>;
-
-    const tableContent = keys.map((key) =>
-      getFormattedTableRow(parentKey, key, jsonData[key])
-    );
-    return (
-      <table key={`${parentKey}.${Math.random()}`}>
-        <thead>
-          <tr key={Math.random()}>
-            <th style={{ width: 175 }}>FIELD</th>
-            <th>VALUE</th>
-          </tr>
-        </thead>
-        <tbody>{tableContent}</tbody>
-      </table>
-    );
-  }
-
-  function getFormattedTableRow(parentKey: string, field: string, value: any) {
-    let clsName = "";
-    if (
-      CommonUtils.jsonValueType(value) === JsonValueType.string ||
-      CommonUtils.jsonValueType(value) === JsonValueType.boolean ||
-      CommonUtils.jsonValueType(value) === JsonValueType.number
-    )
-      clsName = "td-string";
-    let fieldKey = field;
-    if (parentKey !== "") fieldKey = `${parentKey}.${field}`;
-    const formattedVal = getFormattedData(fieldKey, value);
-    return (
-      <tr key={Math.random()}>
-        <td className="td-field td-string">{field}</td>
-        <td>
-          <span className={`value-cell ${clsName}`}>{formattedVal}</span>
-        </td>
-      </tr>
-    );
-  }
   return <div className="json-view">{formattedJsonObject}</div>;
 };
 
