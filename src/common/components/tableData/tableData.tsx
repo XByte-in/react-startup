541111c8a3171e0e548cc147ef86c6b429034807
Pranshu Gupta <54533657+pranshu-bst@users.noreply.github.com>
2023-04-18 20:49:38 +0530

    Updated

---
+++ src/common/components/tableData/tableData.tsx
@@ -0,0 +1,53 @@
+import { ColumnBodyOptions, Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
+import { Paginator } from "primereact/paginator";
+
+export interface IDataTableColumnsParams {
+  field?: string;
+  sortable_header?: string;
+  button?:
+    | React.ReactNode
+    | ((data: any, options: ColumnBodyOptions) => React.ReactNode);
+}
+
+interface IDataTableParams {
+  data: Array<any>;
+  columnsParams: IDataTableColumnsParams[];
+}
+
+const addColumns = (columValues: IDataTableColumnsParams[]) => {
+  return columValues.map((columValue) => {
+    if (!columValue.field) {
+      return <Column body={columValue.button} />;
+    } else {
+      return (
+        <Column
+          field={columValue.field}
+          sortable
+          header={columValue.sortable_header}
+        ></Column>
+      );
+    }
+  });
+};
+
+const TableData = (props: IDataTableParams) => {
+  return (
+    <div>
+      <DataTable
+        value={props.data}
+        responsiveLayout="scroll"
+        paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+        dataKey="id"
+        paginator
+        emptyMessage="No data found."
+        className="datatable-responsive"
+        currentPageReportTemplate="Showing {first} to {last} of {totalRecords} posts"
+        rows={10}
+      >
+        {addColumns(props.columnsParams)}
+      </DataTable>
+    </div>
+  );
+};
+export default TableData;
