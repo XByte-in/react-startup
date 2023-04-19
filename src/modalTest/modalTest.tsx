ef3c569157bd527803466b496d999d8d35b39b7a
Pranshu Gupta <54533657+pranshu-bst@users.noreply.github.com>
2023-04-19 22:07:54 +0530

    Updated

--- src/modalTest/modalTest.tsx
+++ src/modalTest/modalTest.tsx
@@ -1,13 +1,52 @@
 import React from "react";
 import "./modalTest.scss";
 import { IModalComponentParams } from "../common/components/modal/interface";
+import Label from "../common/components/label/label";
+import { TypographyConst } from "../common/scss/typographyConst";
+import { Type } from "../common/commonConst";
+import InputField, {
+  InputFieldType,
+} from "../common/components/inputField/inputField";
+import Select from "react-select/dist/declarations/src/Select";
+import SelectField from "../common/components/selectField/selectField";
 
-interface IModalTestParams extends IModalComponentParams {
-
-}
+interface IModalTestParams extends IModalComponentParams {}
 
 const ModalTest = (props: IModalTestParams) => {
-  return <div className="modal-test"></div>;
+  return (
+    <div className="modal-test">
+      <div className="row">
+        <div className="col label">
+          <Label
+            labelText="Label 1:"
+            typographySize={TypographyConst.body_label}
+            type={Type.default}
+          ></Label>
+        </div>
+        <div className="col data">
+          <InputField type={InputFieldType.text} />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col label">
+          <Label
+            labelText="Label 2:"
+            typographySize={TypographyConst.body_label}
+            type={Type.default}
+          ></Label>
+        </div>
+        <div className="col data">
+          <SelectField
+            className={TypographyConst.body_medium_regular}
+            options={[
+              { label: "label 1", value: "value 1" },
+              { label: "label 2", value: "value 2" },
+            ]}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ModalTest;
