97b7c9a6251c28d2f3b7c50caaa341a13f64095e
Pranshu Gupta <54533657+pranshu-bst@users.noreply.github.com>
2023-07-25 12:11:19 +0530

    updated for reinit

---
+++ src/__test__/App.test.tsx
@@ -0,0 +1,12 @@
+import App from "../App";
+import { it, describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+
+describe("App.js", () => {
+    it("Check if the App render very well", () => {
+        //render our App properly
+        render(<App />);
+        screen.debug();
+    });
+});
