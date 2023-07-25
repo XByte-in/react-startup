2f9b5071625f9a5dbe1566715ac2f02930c03e60
Pranshu Gupta <54533657+pranshu-bst@users.noreply.github.com>
2023-07-25 12:49:47 +0530

    updated for reinit

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,12 +1,11 @@
-import App from "../App";
-import { it, describe } from "vitest";
-import { render, screen } from "@testing-library/react";
+import App from '../App';
+import { it, describe } from 'vitest';
+import { render, screen } from '@testing-library/react';
 
-
-describe("App.js", () => {
-    it("Check if the App render very well", () => {
-        //render our App properly
-        render(<App />);
-        screen.debug();
-    });
+describe('App.js', () => {
+  it('Check if the App render very well', () => {
+    //render our App properly
+    render(<App />);
+    screen.debug();
+  });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "src/__test__/setup.ts",
-  }
+    environment: 'jsdom',
+    setupFiles: 'src/__test__/setup.ts',
+  },
 });
