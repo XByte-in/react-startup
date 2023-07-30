53b09aed479d4ac2d3d17c87d7ec308df07cd097
Pranshu Gupta <54533657+pranshu-bst@users.noreply.github.com>
2023-07-30 15:06:43 +0530

    Updated for theme

---
+++ src/common/theme/typography/typography.ts
@@ -0,0 +1,9 @@
+export interface Typography {
+  [key: string]: {
+    'text-align': string;
+    'font-size': string;
+    'font-weight': string;
+    'line-height': string;
+    'letter-spacing': string;
+  };
+}
