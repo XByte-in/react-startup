30d19da6282c5e5ff0a57c2fe35dab7d9a7a2b55
Pranshu Gupta <54533657+pranshu-bst@users.noreply.github.com>
2023-08-06 14:44:30 +0530

    Updated for testing

--- src/__test__/testControls/_testControls.tsx
+++ src/__test__/testControls/_testControls.tsx
@@ -1,9 +1,4 @@
 import TestImage from './testImage';
-import TestButton from './testButton';
-import TestLabel from './testLabel';
-import TestIcon from './testIcon';
-import TestCheckBox from './testCheckBox';
-import TestRadioButton from './testRadioButton';
 
 const TestControls = () => {
   return (
