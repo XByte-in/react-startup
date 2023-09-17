d96e9fcc23a9bf761d90b1613d0858cc71af70f6
Pranshu Gupta <pranshu.gupta@bluestacks.com>
2023-09-17 21:30:04 +0530

    Updated for env

--- src/app.tsx
+++ src/app.tsx
@@ -4,11 +4,13 @@ import TestControls from './__test__/testControls/_testControls';
 import { loadColors, loadFonts } from './common/theme';
 import defaultColors from './common/theme/colors/colors.json';
 import defaultTypography from './common/theme/typography/typography.json';
+import { VITE_ENV } from './environment';
 
 import './app.scss';
 
 const App = () => {
   useEffect(() => {
+    console.log(VITE_ENV);
     loadFonts(defaultTypography);
     loadColors(defaultColors);
   }, []);
