91c1ed49236bb8dfcebaee961f8e74c057affe02
Pranshu Gupta <pranshu.gupta@bluestacks.com>
2023-09-17 21:40:43 +0530

    updated for env

--- src/app.tsx
+++ src/app.tsx
@@ -4,13 +4,13 @@ import TestControls from './__test__/testControls/_testControls';
 import { loadColors, loadFonts } from './common/theme';
 import defaultColors from './common/theme/colors/colors.json';
 import defaultTypography from './common/theme/typography/typography.json';
-import { VITE_ENV } from './environment';
+import Environment from './environment';
 
 import './app.scss';
 
 const App = () => {
   useEffect(() => {
-    console.log(VITE_ENV);
+    console.log(Environment.GOOGLE_CLIENT_ID);
     loadFonts(defaultTypography);
     loadColors(defaultColors);
   }, []);
--- src/environment.ts
+++ src/environment.ts
@@ -1,1 +1,4 @@
-export const VITE_ENV = import.meta.env.VITE_ENV;
+export default class Environment {
+  static readonly ENV = import.meta.env.VITE_ENV;
+  static readonly GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+}
