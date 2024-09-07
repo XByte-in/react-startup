ab0c34fc287945ff84c3a52146131a70db363707
Pranshu Gupta <pranshu.gupta@bluestacks.com>
2024-09-07 14:35:34 +0530

    Updated

--- src/dashboard/header/header.tsx
+++ src/dashboard/header/header.tsx
@@ -1,23 +1,38 @@
+import { useSelector } from 'react-redux';
+
+import { Type } from '../../common/const';
 import GoogleProfile from '../../common/controls/google/googleProfile/googleProfile';
 import Image from '../../common/controls/image/image';
-import type { RootState } from '../../common/store/store';
-import { useSelector } from 'react-redux';
+import Label from '../../common/controls/label/label';
+import { Typography } from '../../common/theme/typography/typography';
+import Environment from '../../environment';
+
 import './header.scss';
+
+import type { RootState } from '../../common/store/store';
 const Header = () => {
   const userEmail = useSelector(
     (state: RootState) => state.googleUserInfo.email
   );
   return (
     <div className="header">
-      <div className="logo">
-        <Image
-          styleObj={{ height: '4rem' }}
-          imageSrc="https://cdn-www.bluestacks.com/bs-images/logo-icon.png"
-        />
-        <Image
-          styleObj={{ height: '3rem', marginLeft: '1rem' }}
-          imageSrc="https://cdn-www.bluestacks.com/bs-images/logo-text.png"
-        />
+      <div className="env-logo">
+        <Label
+          className="env"
+          textId={Environment.ENV}
+          typography={Typography.title_header}
+          type={Type.primary}
+        ></Label>
+        <div className="logo">
+          <Image
+            styleObj={{ height: '4rem' }}
+            imageSrc="https://cdn-www.bluestacks.com/bs-images/logo-icon.png"
+          />
+          <Image
+            styleObj={{ height: '3rem', marginLeft: '1rem' }}
+            imageSrc="https://cdn-www.bluestacks.com/bs-images/logo-text.png"
+          />
+        </div>
       </div>
       {userEmail && <GoogleProfile />}
     </div>
