951104b8f10d8654848623fe583c2150e8d1dab6
Pranshu Gupta <pranshu.gupta@bluestacks.com>
2024-11-02 15:44:12 +0530

    Updated

--- src/common/controls/query/query.tsx
+++ src/common/controls/query/query.tsx
@@ -1,3 +1,6 @@
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { Type, Size } from '../../const';
+import Button from '../button/button';
 import InputField from '../inputField/inputField';
 import { InputFieldType } from '../inputField/inputFieldType';
 import './query.scss';
@@ -6,12 +9,21 @@ const Query = () => {
     document.getElementById('queryInput')?.focus();
   };
   return (
-    <div className="query" onClick={handleClick}>
-      <InputField
-        id="queryInput"
-        className="query-text"
-        placeholder="Query"
-        type={InputFieldType.text}
+    <div className="query">
+      <div className="query-fields" onClick={handleClick}>
+        <InputField
+          id="queryInput"
+          className="query-text"
+          placeholder="Query"
+          type={InputFieldType.text}
+        />
+      </div>
+      <Button
+        textId="query"
+        className="query-button"
+        type={Type.primary}
+        size={Size.medium}
+        fontAwesomeIcon={faMagnifyingGlass}
       />
     </div>
   );
